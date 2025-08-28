04473c957f28593a60477edd076c4fd9f5f02a80
vigneshwaran052575 <vickymaruthai27@gmail.com>
2025-08-28 15:53:11 +0530

    Update Filped .java

--- Filped .java
+++ Filped .java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+public class Filped {
+    public static void main(String[] args) {
+        String filppedLetters = "BCDEHIKOX";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an alphabet: ");
+        char Letter = scanner.next(). toUpperCase().charAt(0);
+        if(filppedLetters.indexOf(Letter)!=-1) {
+            System.out.println("The alphabet is filpped.");
+        } else {
+            System.out.println("The alphabet is not filpped.");
+        }
+        scanner.close();
+    }
+}
